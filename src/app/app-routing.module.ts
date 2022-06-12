34e82045ad18bf4ed6a97c22087235b3f7ff1081
Paulo Santos III <68072348+PauloSantosIII@users.noreply.github.com>
2022-06-12 17:03:18 -0300

    addding EntryListComponent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  // { path: 'entries', loadChildren: () => import('./pages/entries/entries.module').then(m => m.EntriesModule) },
+  { path: 'entries', loadChildren: './pages/entries/entries.module#EntriesModule' },
   { path: 'categories', loadChildren: './pages/categories/categories.module#CategoriesModule' },
   // { path: 'reports', loadChildren: () => import('./pages/reports/reports.module').then(m => m.ReportsModule) },
 
