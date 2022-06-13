b146e6c30106cd90ac4b26e38ac665ae981d25a7
Paulo Santos III <68072348+PauloSantosIII@users.noreply.github.com>
2022-06-13 01:14:15 -0300

    adding BaseResourceModel

--- src/app/pages/categories/shared/category.model.ts
+++ src/app/pages/categories/shared/category.model.ts
@@ -1,7 +1,9 @@
-export class Category {
+import { BaseRosourceModel } from '../../../shared/models/base-resource.model'
+
+export class Category extends BaseRosourceModel {
   constructor(
     public id?:number,
     public name?: string,
     public description?: string
-  ){}
+  ){ super() }
 }
---
+++ src/app/shared/models/base-resource.model.ts
@@ -0,0 +1,3 @@
+export abstract class  BaseRosourceModel {
+  id?: number
+}
