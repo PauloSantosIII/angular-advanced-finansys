560105c6837984accf5104316f3ed18e948ca241
Paulo Santos III <68072348+PauloSantosIII@users.noreply.github.com>
2022-06-15 11:12:22 -0300

    addin RouterModule to Sharedmodule && improvind PageComponents with BreadCrumbComponent

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 import { BreadCrumbComponent } from './components/bread-crumb/bread-crumb.component'
 
 @NgModule({
@@ -9,7 +10,8 @@ import { BreadCrumbComponent } from './components/bread-crumb/bread-crumb.compon
   ],
   imports: [
     CommonModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    RouterModule
   ],
   exports: [
     CommonModule,
