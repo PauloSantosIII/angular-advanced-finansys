4c490c98f2f7247183debf06c84691475e83d401
Paulo Santos III <68072348+PauloSantosIII@users.noreply.github.com>
2022-06-16 16:34:02 -0300

    adding ResportsModuleComponents

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,9 +4,7 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
   { path: 'entries', loadChildren: './pages/entries/entries.module#EntriesModule' },
   { path: 'categories', loadChildren: './pages/categories/categories.module#CategoriesModule' },
-  // { path: 'reports', loadChildren: () => import('./pages/reports/reports.module').then(m => m.ReportsModule) },
-
-  { path: '', redirectTo: '/reports', pathMatch: 'full' }
+  { path: '', loadChildren: './pages/reports/reports.module#ReportsModule' }
 ];
 
 @NgModule({
--- src/app/pages/entries/shared/entry.service.ts
+++ src/app/pages/entries/shared/entry.service.ts
@@ -1,7 +1,7 @@
 import { Injectable, Injector } from '@angular/core'
 
 import { Observable } from 'rxjs'
-import { catchError, flatMap } from 'rxjs/operators'
+import { catchError, flatMap, map } from 'rxjs/operators'
 
 import { BaseResourceService } from 'src/app/shared/services/base-resource.service'
 import { CategoryService } from './../../categories/shared/category.service'
@@ -27,6 +27,12 @@ export class EntryService extends BaseResourceService<Entry> {
     return this.setCategoryAndSendToServer(entry, super.update.bind(this))
   }
 
+  getByMonthAndYear(month: number, year: number): Observable<Entry[]> {
+    return this.getAll().pipe(
+      map(entries => this.filterByMonthAndYear(entries, month, year))
+    )
+  }
+
   private setCategoryAndSendToServer(entry: Entry, sendFn: any): Observable<Entry> {
     return this.categoryService.getById(entry.categoryId).pipe(
       flatMap(category => {
@@ -35,6 +41,16 @@ export class EntryService extends BaseResourceService<Entry> {
       }),
       catchError(this.handlerError)
     )
+  }
+
+  private filterByMonthAndYear(entries: Entry[], month: number, year: number) {
+    return entries.filter(entry => {
+      const entryDate = moment(entry.date, "DD/MM/YYYY");
+      const monthMatches = entryDate.month() + 1 == month;
+      const yearMatches = entryDate.year() == year;
+
+      if(monthMatches && yearMatches) return entry;
+    })
   }
 
 }
